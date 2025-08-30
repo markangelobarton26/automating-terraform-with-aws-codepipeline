bc02de2d7e16931dd8d91587a7570dd654d51984
Mark Angelo Barton <m.barton@qstrike.com>
2025-08-30 17:20:53 +0800

    First Test

--- demo-project/stage1-basic-ec2/ec2.tf
+++ demo-project/stage1-basic-ec2/ec2.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "web_server" {
               EOF
 
   tags = {
-    Name        = "demo-web-server-stage1"
+    Name        = "bartz-sample"
     Environment = "demo"
     Stage       = "1-basic"
   }
